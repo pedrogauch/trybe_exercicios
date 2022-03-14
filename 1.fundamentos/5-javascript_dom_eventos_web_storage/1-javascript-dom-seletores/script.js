8fa59231553ea68601642abaac03c8261badd73c
PedroGauch <pedro.gauch@gmail.com>
2022-03-14 20:27:04 -0300

    Melhorando o codigo

--- 1.fundamentos/5-javascript_dom_eventos_web_storage/1-javascript-dom-seletores/script.js
+++ 1.fundamentos/5-javascript_dom_eventos_web_storage/1-javascript-dom-seletores/script.js
@@ -1,38 +1,38 @@
 // Exercicio 1
-function changeText() {
-    document.getElementsByTagName('p')[1].innerHTML = "HOJE PERDI 6 HORAS DO MEU DIA."
+function changeText(element, text) {
+    document.getElementsByTagName(element)[1].innerHTML = text;
 }
-changeText();
+changeText('p', "HOJE PERDI 6H DO MEU DIA");
 
 // Exercicio 2
-function changeYellowToGreen() {
-    let yellowToGreen = document.getElementsByClassName('main-content')[0];
-    yellowToGreen.style.background = "rgb(76,164,109)";
+function changeYellowToGreen(nomeDiv, cor) {
+    const yellowToGreen = document.getElementsByClassName(nomeDiv)[0];
+    yellowToGreen.style.background = cor;
 }
-changeYellowToGreen();
+changeYellowToGreen('main-content', 'green');
 
 // Exercicio 3
-function changeRedToWhite() {
-    let redToWhite = document.getElementsByClassName('center-content')[0];
-    redToWhite.style.background = "white";
+function changeRedToWhite(element, color) {
+    let redToWhite = document.getElementsByClassName(element)[0];
+    redToWhite.style.background = color;
 }
-changeRedToWhite();
+changeRedToWhite('center-content', 'white');
 
 // Exercicio 4
-function textCorrector() {
-    let title = document.getElementsByTagName('h1')[0];
-    title.innerHTML = "Exercício 5.1 - JavaScript";
+function textCorrector(element, text) {
+    let title = document.getElementsByTagName(element)[0];
+    title.innerHTML = text;
 } 
-textCorrector();
+textCorrector('h1', 'Exercício 5.1 - JavaScript');
 
 //Exercício 5
-function upperCase() {
+function upperCase(element) {
     let paragrafo = document.getElementsByTagName('p')[0];
     paragrafo.innerHTML = paragrafo.innerHTML.toUpperCase();
 }
-upperCase();
+upperCase('p');
 
-Exercicio 6
+//Exercicio 6
 function showParagraphs() {
     let paragraphs = document.getElementsByTagName('p');
     for (i = 0; i < paragraphs.length; i += 1) {
