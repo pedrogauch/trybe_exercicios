0076b2c39734d5893db27a370e5c5116634505fa
PedroGauch <pedro.gauch@gmail.com>
2022-03-14 18:36:44 -0300

    Qual o sentido de perder 4h para depois ter que copiar um gabarito?

--- 1.fundamentos/5-javascript_dom_eventos_web_storage/1-javascript-dom-seletores/script.js
+++ 1.fundamentos/5-javascript_dom_eventos_web_storage/1-javascript-dom-seletores/script.js
@@ -2,4 +2,11 @@
 function changeText() {
     document.getElementsByTagName('p')[1].innerHTML = "VAI TOMAR NO CU ESSE CURSO DE BOSTA."
 }
-changeText()
+changeText();
+
+// Exercicio 2
+function changeYellowToGreen() {
+    let yellowToGreen = document.getElementsByClassName('main-content')[0];
+    yellowToGreen.style.background = "rgb(76,164,109)";
+}
+changeYellowToGreen();
